a21f2af1004d17a8b557c29d3e601b541b9d752f
Ryan S <planetlucid@gmail.com>
2019-09-15 19:43:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,6 +5,9 @@ class Song
   @@count = 0
   @@genre_count = 0
   @@artist_count = 0
+  @@genres=[]
+  @@artists=[]
+ 
  
   def initialize
     @@count += 1
@@ -12,4 +15,10 @@ class Song
     @@artist_count += 1
   end
 
+  def self.count
+    @@count
+    @@genre_count
+    @@artist_count
+  end
+  
 end
